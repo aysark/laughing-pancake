065c98b6bfc1031213374c2de56b221829e6510e
Aysar Khalid <ak@akpro.net>
2017-05-06 17:07:15 -0700

    added basic rating

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,49 @@ import React, { Component } from 'react';
 
 import 'semantic-ui-css/semantic.min.css';
 
-import { Grid, Container, Header, Menu, Segment, Icon } from 'semantic-ui-react'
+import { Grid, Container, Header, Menu, Segment, Icon, Button, Progress, Rating } from 'semantic-ui-react'
 
 class App extends Component {
   state = {
-    activeItem: 'survey'
+    activeItem: 'survey',
+    percent: 0
+
   }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
+  increment = () => this.setState({
+   percent: this.state.percent >= 100 ? 0 : this.state.percent + 20,
+  })
+
+  handleRate = (e, { rating, maxRating }) => {
+    this.setState({ rating, maxRating })
+    this.setState({ percent: 50 })
+  }
+
+  renderSecondaryQuestion() {
+    if (this.state.rating > 0 && this.state.rating < 3) {
+      return (
+          <h2>Sorry to hear that, What caused this?</h2>
+      );
+    } else if (this.state.rating > 3) {
+      return (
+        <h2>Great to hear!  What was the reason?</h2>
+      );
+    } else {
+      return (
+        <p></p>
+      );
+    }
+  }
+
   render() {
     const { activeItem } = this.state
-
     const BodyContainer = () => (
       <Container text>
-        <Header as='h2'>Header</Header>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
+        <Header as='h2'>How was work today?</Header>
+         <Rating maxRating={5} defaultRating={0} icon='star' size='massive' onRate={this.handleRate} />
+         { this.renderSecondaryQuestion() }
       </Container>
     )
 
@@ -35,6 +62,12 @@ class App extends Component {
               <Menu.Item name='survey'  active={activeItem === 'survey'} onClick={this.handleItemClick} />
               <Menu.Item name='profile'  active={activeItem === 'profile'} onClick={this.handleItemClick} />
               <Menu.Item name='contact'  active={activeItem === 'contact'} onClick={this.handleItemClick} />
+
+              <Menu.Item position='right'>
+                <div className="ProgressBar">
+                  <Progress percent={this.state.percent} progress size='medium'/>
+                </div>
+              </Menu.Item>
             </Menu>
           </Segment>
           </Container>
